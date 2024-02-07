913edb6d8c8c68bfc568e0c0ec1ce0c576712d31
Tejaswi Gowda <tejaswi@asu.edu>
2024-02-07 10:46:48 -0700

    in class

--- rssReader/server.js
+++ rssReader/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 1234;
+var port = 8080;
 
 app.get("/", function (req, res) {
     res.redirect("index.html")
